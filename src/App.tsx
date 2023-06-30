75e0f85655a43b39b38fb8f62baecc0cb2f9fdce
KISUPM <panzer_thai@hotmail.com>
2023-06-30 10:57:00 +0700

    update index path

--- src/App.tsx
+++ src/App.tsx
@@ -63,12 +63,12 @@ function App() {
         } */}
 
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
 
           <Route
-            path="/company"
+            path="/"
             element={
               <Project>
                 <PageCompany />
