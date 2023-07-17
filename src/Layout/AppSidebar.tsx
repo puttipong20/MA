8a2de4b1fbf3b52d037b45c9b9c7d8e0a76b0f4d
KISUPM <panzer_thai@hotmail.com>
2023-07-17 09:32:29 +0700

    update ui sidebar

--- src/Layout/AppSidebar.tsx
+++ src/Layout/AppSidebar.tsx
@@ -15,6 +15,7 @@ import {
   Flex,
   InputLeftElement,
   InputGroup,
+  HStack,
   // IconButton,
   // useColorMode,
 } from "@chakra-ui/react";
@@ -41,6 +42,9 @@ import { SearchIcon } from "@chakra-ui/icons";
 import { LuLogOut } from "react-icons/lu";
 import { signOut } from "firebase/auth";
 import { AuthContext } from "../context/AuthContext";
+import { HiOutlineOfficeBuilding } from "react-icons/hi";
+import { BsDot } from "react-icons/bs";
+import { ImOffice } from "react-icons/im";
 // import { FaLightbulb } from "react-icons/fa";
 
 interface Props {
@@ -169,7 +173,7 @@ const Sidebar: React.FC<Props> = (props) => {
         transition={"all 0.3s"}
         // _hover={{ textShadow: "0px 0px 30px #fff" }}
         _hover={{ textShadow: "0px 0px 30px #000" }}
-        // border="1px solid black"
+      // border="1px solid black"
       >
         CRAFTING LAB
       </Heading>
@@ -188,14 +192,14 @@ const Sidebar: React.FC<Props> = (props) => {
           />
         </InputGroup>
       </Box>
-      <Divider my="5px" opacity={"1"} />
+      {/* <Divider my="5px" opacity={"1"} /> */}
       {isFetching ? (
         <Flex w="100%" justify={"center"} align={"center"}>
           <Spinner />
         </Flex>
       ) : (
         <Box>
-          <Accordion defaultIndex={[openIndex]}>
+          <Accordion allowToggle defaultIndex={[openIndex]}>
             {filterCompany.map((i, index) => {
               // if (params["company"] === i.companyId) { Company.setCompany(i.companyId, i.detail.companyName) }
               const focusCompany = params["company"] === i.companyId;
@@ -203,88 +207,105 @@ const Sidebar: React.FC<Props> = (props) => {
                 <AccordionItem
                   key={index}
                   userSelect={"none"}
-                  borderTop="1px solid rgb(0,0,0,0.1)"
+                  borderTop="0px solid rgb(0,0,0,0.1)"
                   borderBottom="1px solid rgb(0,0,0,0.1)"
                   transition="all 0.3s"
-                  _hover={{ bg: color }}
-                  borderRadius={"10px"}
-                  bg={focusCompany ? color : ""}
+                  _hover={{ bg: "#fff" }}
+                  // borderRadius={"10px"}
+                  // bg={focusCompany ? color : ""}
                   my="5px"
-                  // borderLeft={focusCompany ? "3px solid white" : "none"}
+                // borderLeft={focusCompany ? "3px solid white" : "none"}
                 >
                   <AccordionButton
                     onClick={() => {
                       navigate(`/company/${i.companyId}`);
                     }}
+                    _hover={{ bg: "#fff" }}
                   >
-                    <Flex justify={"space-between"}>
+                    <HStack justify={"flex-start"} alignItems={"flex-start"} color={focusCompany ? "#4c7bf4" : "black"}>
+                      <Text mt="0.25rem">
+                        <HiOutlineOfficeBuilding />
+                        {/* <ImOffice /> */}
+                      </Text>
                       <Text
-                        fontWeight={focusCompany ? "bold" : "normal"}
+                        fontWeight={"normal"}
                         textAlign={"left"}
                       >
                         {i.detail.companyName}
                       </Text>
                       {/* <AccordionIcon /> */}
-                    </Flex>
+                    </HStack>
                   </AccordionButton>
-                  <AccordionPanel>
-                    <VStack fontSize={"0.9rem"} align={"left"} pl="5%">
-                      {i.detail.projects !== undefined &&
-                      i.detail.projects.filter((i) => i.status === "enable")
-                        .length !== 0 ? (
-                        i.detail.projects?.map((j, index) => {
-                          // if (params["projectID"] === j.id) { Company.setProject(j.id, j.projectName) }
-                          const focusProject = params["projectID"] === j.id;
-                          if (j.status === "enable") {
-                            return (
-                              <Text
-                                key={index}
-                                fontWeight={focusProject ? "bold" : "normal"}
-                                cursor={"pointer"}
-                                transition={"all 0.1s"}
-                                borderRadius={"5px"}
-                                pl="1rem"
-                                bg={focusProject ? color : "none"}
-                                _hover={{ fontWeight: "bold", bg: color }}
-                                position="relative"
-                                onClick={() => {
-                                  // console.log(i.detail.companyName)
-                                  // console.log(j.projectName)
-                                  navigate(
-                                    `/company/${i.companyId}/${j.id}/${j.projectName}/problemReport`
-                                  );
-                                }}
-                              >
-                                <Text
-                                  as="span"
-                                  position="absolute"
-                                  left="0.5rem"
+                  {
+                    i.detail.projects?.length != 0 &&
+                    <AccordionPanel pt="0px">
+                      <VStack mt="-0.5rem" fontSize={"0.9rem"} align={"left"} pl="5%">
+                        {i.detail.projects !== undefined &&
+                          i.detail.projects.filter((i) => i.status === "enable")
+                            .length !== 0 ? (
+                          i.detail.projects?.map((j, index) => {
+                            // if (params["projectID"] === j.id) { Company.setProject(j.id, j.projectName) }
+                            const focusProject = params["projectID"] === j.id;
+                            if (j.status === "enable") {
+                              return (
+                                <Box
+                                  h="20px"
+                                  key={index}
+                                  // fontWeight={focusProject ? "bold" : "normal"}
+                                  cursor={"pointer"}
+                                  transition={"all 0.1s"}
+                                  borderRadius={"5px"}
+                                  // pl="1rem"
+                                  color={focusProject ? "rgb(76, 123, 244)" : "black"}
+                                  // _hover={{ fontWeight: "bold", bg: color }}
+                                  position="relative"
+                                  onClick={() => {
+                                    // console.log(i.detail.companyName)
+                                    // console.log(j.projectName)
+                                    navigate(
+                                      `/company/${i.companyId}/${j.id}/${j.projectName}/problemReport`
+                                    );
+                                  }}
                                 >
-                                  -
-                                </Text>
-                                {j.projectName}
-                              </Text>
-                            );
-                          }
-                        })
-                      ) : (
-                        <Text fontWeight={"normal"} textAlign={"center"}>
-                          ยังไม่มีข้อมูล Project ของบริษัทนี้
-                        </Text>
-                      )}
-                      <AddProject
+                                  <HStack
+                                    // as="span"
+                                    // position="absolute"
+                                    // left="0.5rem"
+                                    spacing={0}
+                                  >
+                                    <Text fontSize={"40px"}>
+                                      <BsDot />
+                                    </Text>
+                                    <Text ml="-0.5rem">
+                                      {j.projectName}
+                                    </Text>
+                                  </HStack>
+                                </Box>
+
+                              );
+                            }
+                          })
+                        ) : (
+                          <></>
+                          // <Text fontWeight={"normal"} textAlign={"center"}>
+                          //   ยังไม่มีข้อมูล Project ของบริษัทนี้
+                          // </Text>
+                        )}
+                        {/* <AddProject
                         companyId={i.companyId}
                         companyName={i.detail.companyName}
-                      />
-                    </VStack>
-                  </AccordionPanel>
+                      /> */}
+                        {/* <Text pl="0rem" color="#4c7bf4" cursor={"pointer"}>+ เพิ่ม Project</Text> */}
+                      </VStack>
+                    </AccordionPanel>
+                  }
                 </AccordionItem>
               );
             })}
           </Accordion>
         </Box>
       )}
-      <Divider my="5px" opacity={"1"} />
+      {/* <Divider my="5px" opacity={"1"} /> */}
       <Box
         w="100%"
         p="0.5rem"
