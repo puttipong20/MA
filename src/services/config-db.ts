895d0c8a11355b50d8475d77c4d353044dbe93ff
KISUPM <panzer_thai@hotmail.com>
2023-09-25 16:52:44 +0700

    add env

--- src/services/config-db.ts
+++ src/services/config-db.ts
@@ -8,7 +8,6 @@ import { getStorage, connectStorageEmulator } from "firebase/storage";
 const config = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
@@ -21,11 +20,11 @@ const auth = getAuth();
 const db = getFirestore();
 const storage = getStorage();
 
-if (window.location.hostname === "localhost") {
-  connectAuthEmulator(auth, "http://localhost:9099");
-  connectFunctionsEmulator(functions, "localhost", 5001);
-  connectFirestoreEmulator(db, "localhost", 8080);
-  connectStorageEmulator(storage, "localhost", 9199);
-}
+// if (window.location.hostname === "localhost") {
+//   connectAuthEmulator(auth, "http://localhost:9099");
+//   connectFunctionsEmulator(functions, "localhost", 5001);
+//   connectFirestoreEmulator(db, "localhost", 8080);
+//   connectStorageEmulator(storage, "localhost", 9199);
+// }
 
 export { functions, auth, storage, db };
